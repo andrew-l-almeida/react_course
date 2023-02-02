9d3c06e3c212b3c29ca2db01a48cea77938390d2
Andrew Almeida <andrew.l.almeida@outook.com>
2023-02-01 22:01:09 -0300

    Using dinamic outputs with variables

--- 03_React_Basics/src/components/ExpenseItem.js
+++ 03_React_Basics/src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import './ExpenseItem.css'
-function ExpenseItem(){
+function ExpenseItem() {
+    const expenseDate = new Date(2023, 1, 1);
+    const expenseTitle = 'Car Insurance';
+    const expensePrice = 293.67
     return (
         <div className='expense-item'>
-            <div>01/02/2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expensePrice}</div>
             </div>
         </div>
     )
