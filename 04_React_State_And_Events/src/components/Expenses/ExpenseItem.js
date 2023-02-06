52783350ec6cd7337173bebaa836452267df3636
Andrew Almeida <andrew.l.almeida@outook.com>
2023-02-06 20:30:41 -0300

    Learned how to use useState

--- 04_React_State_And_Events/src/components/Expenses/ExpenseItem.js
+++ 04_React_State_And_Events/src/components/Expenses/ExpenseItem.js
@@ -2,15 +2,18 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
+import {useState} from 'react'
+
 const ExpenseItem = props => {
+    const [title, setTitle] = useState(props.title) 
     const clickHandler = () =>{
-        console.log('clicked')
+        setTitle('Updated!!!')
     }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
