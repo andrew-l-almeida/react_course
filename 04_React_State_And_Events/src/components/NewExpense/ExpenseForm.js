4a35aa438dd69fee7dbee7a532df4f105a81b007
Andrew Almeida <andrew.l.almeida@outook.com>
2023-02-07 14:01:26 -0300

    Child-to-Parent component communication

--- 04_React_State_And_Events/src/components/NewExpense/ExpenseForm.js
+++ 04_React_State_And_Events/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
@@ -29,9 +29,11 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate)
         }
-        console.log(expensesData)
-        clearForm()
+        props.onSaveExpenseData(expensesData)
+        clearForm();
+
     }
+
 return(
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
