2d0a814edcda4a9b5d5403dc620312c0b3970808
Andrew Almeida <andrew.l.almeida@outook.com>
2023-03-22 20:48:11 -0300

    Learned how to use Refs

--- 07_A_Complete_Practice_Project/src/Components/UI/ErrorModal.js
+++ 07_A_Complete_Practice_Project/src/Components/UI/ErrorModal.js
@@ -1,12 +1,15 @@
+import  ReactDOM  from "react-dom";
+
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css"
 
-const ErrorModal = props => {
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}/>
+}
+const Overlay = props => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm}/>
-            <Card className={classes.modal}>
+        <Card className={classes.modal}>
                 <header className={classes.header}>
                     <h2>{props.title}</h2>
                 </header>
@@ -17,7 +20,15 @@ const ErrorModal = props => {
                     <Button onClick={props.onConfirm} >Okay</Button>
                 </footer>
             </Card>
-        </div>
+    )
+}
+
+const ErrorModal = props => {
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<Overlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>, document.getElementById('overlay-root'))}
+        </>
     )
 };
 
