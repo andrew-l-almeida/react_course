6cc31eafa15bbac9b7ca783fd31c3e61b5047cf4
Andrew Almeida <andrew.l.almeida@outook.com>
2023-04-10 22:39:19 -0300

    Make the items acumulative

--- 09_Food_Order_Practice/src/Components/Store/CartProvider.js
+++ 09_Food_Order_Practice/src/Components/Store/CartProvider.js
@@ -8,9 +8,24 @@ const defaultCartState = {
 }
 
 const cartReducer = (state, actions) => {
-    if(actions.type === 'ADD'){
-        const updatedItems = state.items.concat(actions.item)
+    if (actions.type === 'ADD') {
+        
         const updatedTotalAmount = state.totalAmount + actions.item.price * actions.item.amount;
+        
+        const existingCartIndex = state.items.findIndex(item => item.id === actions.item.id)
+        const existingCartItem = state.items[existingCartIndex]
+        let updatedItems;
+        
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + actions.item.amount
+            };
+            updatedItems = [...state.items]
+            updatedItems[existingCartIndex] = updatedItem
+        } else { 
+             updatedItems = state.items.concat(actions.item)
+        }
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -20,10 +35,10 @@ const cartReducer = (state, actions) => {
 }
 
 const CartProvider = props => {
-const [cartState, dispatchCartActions] = useReducer(cartReducer, defaultCartState)
+    const [cartState, dispatchCartActions] = useReducer(cartReducer, defaultCartState)
 
     const addItemtoCartHandler = item => {
-        dispatchCartActions({type: 'ADD', item: item})
+        dispatchCartActions({ type: 'ADD', item: item })
     }
     const removeItemFromCartHandler = id => {
 
@@ -35,7 +50,7 @@ const [cartState, dispatchCartActions] = useReducer(cartReducer, defaultCartStat
         removeItem: removeItemFromCartHandler
 
     }
-    return(
+    return (
         <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>
     )
 }
